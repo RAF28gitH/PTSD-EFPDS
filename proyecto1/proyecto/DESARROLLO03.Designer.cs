aa4a1834bac02d3aecb1f04c6ffe3eb21e86fb2d
sharkjirA <gumf080213hgtzrra1@cbtis217.edu.mx>
2024-10-25 12:56:21 -0600

    fondo

--- proyecto1/proyecto/DESARROLLO03.Designer.cs
+++ proyecto1/proyecto/DESARROLLO03.Designer.cs
@@ -129,6 +129,8 @@ namespace proyecto
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
 			this.ClientSize = new System.Drawing.Size(731, 429);
 			this.Controls.Add(this.pictureBox3);
 			this.Controls.Add(this.label3);
