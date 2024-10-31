6293c7405be3ba7bee7965ebae6770be7e7b3ffd
RAF28gitH <raf28elnooob@gmail.com>
2024-10-31 17:31:52 +0100

    agregado

--- proyecto1/proyecto/EncuestaSatisfaccion.Designer.cs
+++ proyecto1/proyecto/EncuestaSatisfaccion.Designer.cs
@@ -36,12 +36,29 @@ namespace proyecto
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(31, 26);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(152, 31);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Encuesta de satisfacción";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
 			// 
 			// EncuestaSatisfaccion
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "EncuestaSatisfaccion";
+			this.ClientSize = new System.Drawing.Size(215, 79);
+			this.Controls.Add(this.button1);
 			this.Name = "EncuestaSatisfaccion";
+			this.Text = "EncuestaSatisfaccion";
+			this.ResumeLayout(false);
 		}
+		private System.Windows.Forms.Button button1;
 	}
 }
